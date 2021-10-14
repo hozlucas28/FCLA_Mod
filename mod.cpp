a4e53cf70bdceb2000c0866195f8435ccc68854d
hozlucas28 <hozlucas28@gmail.com>
2021-10-14 14:52:40 -0300

    Update

--- mod.cpp
+++ mod.cpp
@@ -1,14 +1,17 @@
-name = "Community Base Addons v3.15.5";
-picture = "logo_cba_ca.paa";
-actionName = "Website";
-action = "https://github.com/CBATeam/CBA_A3/wiki";
-description = "Bugtracker: https://github.com/CBATeam/CBA_A3/issues<br />Documentation: https://cbateam.github.io/CBA_A3/docs/files/overview-txt.html";
-logo = "logo_cba_ca.paa";
-logoOver = "logo_cba_ca.paa";
-tooltip = "Community Base Addons";
-tooltipOwned = "Community Base Addons Owned";
-overview = "What does the name Community Base Addons mean? It is a system that offers a range of features for addon-makers and mission designers. This includes a collection of community-built functions, an integrated keybinding system, and extend event-handler support for compatibility with other 3rd-party addons; and much much more.";
-author = "CBA Team";
-overviewPicture = "logo_cba_ca.paa";
-overviewText = "Community Base Addons overviewText";
-overviewFootnote = "<br /><br /><t color='#999999'>This content is under GPLv2 License.<br />Press <t /><t color='#19d3ff'>Left Shift + P<t /><t color='#999999'> to open the store page for more information.<t />";
+
+/********************************************************************************|
+|                              PRESENTACIÓN DEL MOD                              |
+|********************************************************************************/
+
+name = "FCLA Mod";
+logo = "logo.paa";
+author = "hozlucas28";
+action = "https://github.com/hozlucas28/FCLA_Mod";
+picture = "picture.paa";
+overview = "¡FCLA Mod es un mod de uso privado para la comunidad de Arma 3 FCLA! <br/>Incluye nuevas funcionalidades para mejorar la experiencia del juego, ademas de funciones públicas que pueden ser de utilidad en diversas situaciones.<br/><br/>• Wiki: <a href='https://github.com/hozlucas28/FCLA_Mod/wiki'>https://github.com/hozlucas28/FCLA_Mod/wiki</a> <br/>• Página de la comunidad: <a href='https://arma3clanfcla.wixsite.com/fcla'>https://arma3clanfcla.wixsite.com/fcla</a>";
+logoOver = "logoOver.paa";
+logoSmall = "logoSmall.paa";
+actionName = "GITHUB";
+tooltipOwned = "¡FCLA Mod es un mod de uso privado para la comunidad de Arma 3 FCLA! <br/>Incluye nuevas funcionalidades para mejorar la experiencia del juego, ademas de funciones públicas que pueden ser de utilidad en diversas situaciones.";
+hideName = 0;
+hidePicture = 0;
