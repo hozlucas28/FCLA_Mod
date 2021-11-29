8adc4b16947d4aacbf8e3cfbcbdff4ce79f62a5a
hozlucas28 <hozlucas28@gmail.com>
2021-11-29 12:07:18 -0300

    Update

--- addons/FCLA_Land/CIVILIAN/CfgVehicles.hpp
+++ addons/FCLA_Land/CIVILIAN/CfgVehicles.hpp
@@ -2,3 +2,135 @@
 /********************************************************************************|
 |                              VEHÍCULOS - CIVILES                               |
 |********************************************************************************/
+
+
+
+/* -------------------------- AMBULANCIA - ALTIS --------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Altis: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Altis)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Altis.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Altis", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Altis {
+      author = "hozlucas28";
+      displayName = "Altis";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Altis_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Altis_2.paa"};
+    };
+  };
+};
+
+
+
+/* -------------------------- AMBULANCIA - MALDEN -------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Malden: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Malden)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Malden.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Malden", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Malden {
+      author = "hozlucas28";
+      displayName = "Malden";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Malden_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Malden_2.paa"};
+    };
+  };
+};
+
+
+
+/* -------------------------- AMBULANCIA - TANOA --------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Tanoa: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Tanoa)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Tanoa.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Tanoa", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Tanoa {
+      author = "hozlucas28";
+      displayName = "Tanoa";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Tanoa_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Tanoa_2.paa"};
+    };
+  };
+};
+
+
+
+/* ------------------------- AMBULANCIA - LIVONIA -------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Livonia: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Livonia)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Livonia.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Livonia", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Livonia {
+      author = "hozlucas28";
+      displayName = "Livonia";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Livonia_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Livonia_2.paa"};
+    };
+  };
+};
+
+
+
+/* ------------------------ AMBULANCIA - CHERNARUS ------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Chernarus: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Chernarus)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Chernarus.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Chernarus", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Chernarus {
+      author = "hozlucas28";
+      displayName = "Chernarus";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Chernarus_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Chernarus_2.paa"};
+    };
+  };
+};
+
+
+
+/* ------------------------- AMBULANCIA - TAKISTAN ------------------------- */
+
+class FCLA_C_Van_02_medevac_F_Takistan: C_Van_02_medevac_F {
+  displayName = "Furgoneta (ambulancia - Takistan)";
+  //editorPreview = "\FCLA_Land\CIVILIAN\data\pictures\FCLA_C_Van_02_medevac_F_Takistan.jpg";
+  scope = 2;
+  scopeCurator = 2;
+
+  textureList[] = {"Takistan_White", 1};
+  animationList[] = {"beacon_front_hide", 0, "beacon_rear_hide", 0, "LED_lights_hide", 0, "reflective_tape_hide", 1, "sidesteps_hide", 0};
+  class TextureSources {
+    class Takistan_White {
+      author = "hozlucas28";
+      displayName = "Takistan (blanco)";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Takistan_White_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Takistan_White_2.paa"};
+    };
+
+    class Takistan_Bronze {
+      author = "hozlucas28";
+      displayName = "Takistan (bronce)";
+      textures[] = {"\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Takistan_Bronze_1.paa", "\a3\soft_f_orange\van_02\data\van_wheel_co.paa", "\FCLA_Land\CIVILIAN\data\FCLA_C_Van_02_medevac_F_Takistan_Bronze_2.paa"};
+    };
+  };
+};
