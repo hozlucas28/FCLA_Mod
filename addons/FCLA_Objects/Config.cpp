b625bd02e904b721c86f7a47894e64edce086e66
hozlucas28 <hozlucas28@gmail.com>
2022-01-01 18:13:04 -0300

    Update

--- addons/FCLA_Objects/Config.cpp
+++ addons/FCLA_Objects/Config.cpp
@@ -28,19 +28,39 @@ class CfgPatches {
 
 
 /********************************************************************************|
+|                             EDITOR - SUBCATEGORÍAS                             |
+|********************************************************************************/
+
+class CfgEditorSubcategories {
+	class FCLA_Props {displayName = "Utilería";};
+};
+
+
+
+/********************************************************************************|
 |                                    VEHÍCULOS                                   |
 |********************************************************************************/
 
 class CfgVehicles {
 	class All;
+	class Thing: All {};
 	class Static: All {};
+	class ThingX: Thing {};
 	class AllVehicles: All {};
+	class Building: Static {};
 	class Land: AllVehicles {};
+	class Strategic: Building {};
+	class Items_base_F: ThingX {};
+	class NonStrategic: Building {};
+	class Furniture_base_F: ThingX {};
+	class BagFence_base_F: Strategic {};
+	class Helper_Base_F: NonStrategic {};
+	class UserTexture1m_F: NonStrategic {};
+	class UserTexture10m_F: UserTexture1m_F {};
+	class Furniture_Residental_base_F: Furniture_base_F {};
 	class Man: Land {};
 	class Animal: Man {};
-	class Building: Static {};
 	class Animal_Base_F: Animal {};
-	class NonStrategic: Building {};
 	class Dog_Base_F: Animal_Base_F {};
 	class Fowl_Base_F: Animal_Base_F {};
 	class Alsatian_Base_F: Dog_Base_F {};
@@ -50,5 +70,5 @@ class CfgVehicles {
 
 	#include "\FCLA_Objects\Animals\CfgVehicles.hpp"
 	#include "\FCLA_Objects\Banners\CfgVehicles.hpp"
-	#include "\FCLA_Objects\Decorations\CfgVehicles.hpp"
+	#include "\FCLA_Objects\Props\CfgVehicles.hpp"
 };
---
+++ addons/FCLA_Objects/Props/CfgVehicles.hpp
@@ -0,0 +1,192 @@
+
+/***************************************************************************|
+|                           VEHÍCULOS - "UTILERÍA"                          |
+|***************************************************************************/
+
+
+
+/* ------------------------------- TEXTURAS -------------------------------- */
+
+class FCLA_RainDrops: Helper_Base_F {
+  displayName = "Gotas de lluvia";
+  icon = "iconObject_1x1";
+  model = "\A3\data_f\RainDrop.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Small_Bullet_Holes.jpg";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+};
+
+
+class FCLA_Small_Bullet_Holes: UserTexture1m_F	{
+  author = "hozlucas28";
+  displayName = "Agujeros de bala (pequeños)";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Small_Bullet_Holes.jpg";
+  hiddenSelectionsTextures[] = {"\A3\structures_f_enoch\military\Training\Data\targets_A_CO.paa"};
+  hiddenSelectionsMaterials[] = {"\A3\structures_f_enoch\military\Training\Data\targets.rvmat"};
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+};
+
+
+class FCLA_Planks_v1: Items_base_F	{
+  author = "hozlucas28";
+  displayName = "Barricada de tablas (v1)";
+  model = "\A3\structures_f\data\DoorLocks\planks_1.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Planks_v1.jpg";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    init = "''";
+  	eden = 0;
+    verticalOffset = -5;
+  	verticalOffsetWorld = -5;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+class FCLA_Planks_v2: Items_base_F	{
+  author = "hozlucas28";
+  displayName = "Barricada de tablas (v2)";
+  model = "\A3\structures_f\data\DoorLocks\planks_2.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Planks_v2.jpg";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    init = "''";
+  	eden = 0;
+    verticalOffset = -5;
+  	verticalOffsetWorld = -5;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+class FCLA_Huge_Bullet_Holes_v1: UserTexture10m_F {
+  author = "hozlucas28";
+  displayName = "Agujeros de bala (grandes, v1)";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Huge_Bullet_Holes_v1.jpg";
+  hiddenSelectionsTextures[] = {"\A3\missions_f_tacops\Campaign_1\Missions\to_c01m02.Altis\HOUSE_D2.paa"};
+  vehicleClass = "Signs";
+  destrType = "DestructNo";
+  mapSize = 10;
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    init = "''";
+  	eden = 0;
+    verticalOffset = 0.0060000001;
+  	verticalOffsetWorld = 0;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+class FCLA_Huge_Bullet_Holes_v2: FCLA_Huge_Bullet_Holes_v1 {
+  author = "hozlucas28";
+  displayName = "Agujeros de bala (grandes, v2)";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Huge_Bullet_Holes_v2.jpg";
+  hiddenSelectionsTextures[] = {"\A3\missions_f_tacops\Campaign_1\Missions\to_c01m02.Altis\HOUSE_HOLE.PAA"};
+  mapSize = 10;
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    init = "''";
+  	eden = 0;
+    verticalOffset = 0.0060000001;
+    verticalOffsetWorld = 0;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+
+/* -------------------------------- OBJETOS -------------------------------- */
+
+class FCLA_Short_Sandbags: BagFence_base_F	{
+  author = "hozlucas28";
+  displayName = "Muro de sacos (corto, verde)";
+  icon = "iconObject_1x1";
+  model = "\A3\structures_f\data\DoorLocks\sandbags_1.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Short_Sandbags.jpg";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  reversed = 1;
+  mapSize = 1.98;
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    eden = 0;
+    init = "''";
+    verticalOffset = 0.41499999;
+  	verticalOffsetWorld = 0;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+class FCLA_Piano: Furniture_Residental_base_F {
+  author = "hozlucas28";
+  displayName = "Piano";
+  icon = "iconObject_1x2";
+  model = "\A3\structures_f_enoch\furniture\decoration\piano\piano.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Piano.jpg";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  mapSize = 2.26;
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    eden = 0;
+    init = "''";
+    verticalOffset = 0.456;
+    verticalOffsetWorld = 0;
+    hide[] = {};
+    animate[] = {};
+  };
+};
+
+
+class FCLA_Christmas_Tree: Strategic {
+  author = "hozlucas28";
+  displayName = "Arbol de navidad";
+  model = "\a3\vegetation_f_enoch\Tree\t_PiceaAbiesNativitatis_2s.p3d";
+  //editorPreview = "\FCLA_Objects\Props\pictures\FCLA_Christmas_Tree.jpg";
+  nameSound = "obj_tree";
+  editorCategory = "EdCat_Things";
+  editorSubcategory = "FCLA_Props";
+  scope = 2;
+  scopeCurator = 2;
+
+  class SimpleObject {
+    init = "''";
+  	eden = 0;
+    verticalOffset = 0.24699999;
+  	verticalOffsetWorld = 0;
+  	hide[] = {};
+    animate[] = {};
+  };
+};
