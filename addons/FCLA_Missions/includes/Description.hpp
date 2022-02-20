4c25bc5ea98d3a8299c9281d3550aedfaf345d4e
hozlucas28 <hozlucas28@gmail.com>
2022-02-20 05:53:53 -0300

    Update

--- addons/FCLA_Missions/includes/Description.hpp
+++ addons/FCLA_Missions/includes/Description.hpp
@@ -28,10 +28,22 @@ respawnOnStart = -1;
 
 
 
-/* ------------------------ SEGURIDAD DEL SERVIDOR ------------------------- */
+/* ------------------------------- SEGURIDAD ------------------------------- */
 
 allowFunctionsLog = 0;
 allowFunctionsRecompile = 1;
+
+enableDebugConsole[] = {
+  "76561198304995327", //Ghostkiller.
+  "76561198137876583", //hozlucas28.
+  "76561198260536786", //Mario.
+  "76561198059595506", //Naretick.
+  "76561198173170422", //Sagara.
+  "76561198178496662", //Byron.
+  "76561198010584812", //Kovalsky.
+  "76561197982853635", //Legion.
+  "76561198153898683" //Lorflord.
+};
 
 
 
